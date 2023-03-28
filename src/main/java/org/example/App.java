041387cc364984a79324673f68fbf004fb0d25bb
RelancioBorgesDev <relancio.dev@gmail.com>
2023-03-28 10:12:43 -0300

    Insertion Sort

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,14 +2,20 @@ package org.example;
 
 import org.example.sort.SortAlgos;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main( String[] args ) {
-        int [] vetor = {20,3,5,9,1};
+        int [] vetor = {8,5,1,3};
+        int [] vetor2 = {8,5,1,3};
+        int [] vetor3 = {8,5,1,3};
         SortAlgos sa = new SortAlgos();
-        sa.SelectionSort(vetor);
+        //sa.BubbleSort(vetor);
+        sa.InsertionSort(vetor3);
+       //System.out.println(Arrays.toString(sa.SelectionSort(vetor2)));
     }
 }
--- src/main/java/org/example/sort/SortAlgos.java
+++ src/main/java/org/example/sort/SortAlgos.java
@@ -6,6 +6,7 @@ public class SortAlgos {
         int small = 0;
         int nextValue = 0;
         int pos = 0;
+        int count = 0;
 
         for (int i = 0; i < vetor.length-1; i++){
             small = vetor[i];
@@ -13,12 +14,48 @@ public class SortAlgos {
             for (int j = i+1; j < vetor.length; j++){
                 nextValue = vetor[j];
                 if(nextValue < small){
+                    count++;
                     small = vetor[j];
                     pos = j;
                 }
             }
                 vetor[pos] = vetor[i];
                 vetor[i] = small;
+        }
+        System.out.println("O If foi utilizado: " + count + " vezes.");
+        return vetor;
+    }
+
+    public int [] BubbleSort(int [] vetor){
+        int num1 = 0;
+        int num2 = 0;
+        int count = 0;
+
+        for (int i = 0; i < vetor.length; i++){
+            for (int j = 0; j < vetor.length-i-1; j++){
+                num1 = vetor[j];
+                num2 = vetor[j+1];
+                if(num1 > num2){
+                    count++;
+                    vetor[j] = num2;
+                    vetor[j+1] = num1;
+                }
+            }
+        }
+
+        System.out.println("O If foi utilizado: " + count + " vezes.");
+        return vetor;
+    }
+
+    public int[] InsertionSort(int [] vetor){
+        for(int i = 1; i < vetor.length; i++) {
+            int temp = vetor[i];
+            int count = 0;
+            for (int j = i - 1; j >= 0 && vetor[j] > temp; j--) {
+                vetor[j + 1] = vetor[j];
+                count++;
+            }
+             vetor[i - count] = temp;
         }
 
         return vetor;
